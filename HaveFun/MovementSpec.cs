cedc1c43380f97628cbaa7831235d307025fafc3
John Nicholas <john.nicholas@madetech.com>
2020-05-13 17:29:57 +0100

    automatically commit passing test

--- HaveFun/MovementSpec.cs
+++ HaveFun/MovementSpec.cs
@@ -15,13 +15,21 @@ namespace HaveFun
         [Fact]
         public void AfterRolling5PlayerPositionisAt5()
         {
-            Assert.Equal(5,5);
+            var board = new Board();
+            board.MovePlayer(5);
+            var player1Position = board.Player1Position;
+            Assert.Equal(5,player1Position);
         }
     }
     
     public class Board
     {
-        public int Player1Position { get; } = 0;
+        public int Player1Position { get; private set; } = 0;
+
+        public void MovePlayer(int spaces)
+        {
+            Player1Position = spaces;
+        }
     }
 }
 
