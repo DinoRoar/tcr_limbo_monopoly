32075009b597c1b67e6096092beab28714a328fa
John Nicholas <john.nicholas@madetech.com>
2020-05-13 17:19:44 +0100

    automatically commit passing test

---
+++ HaveFun/CreateBoardSpec.cs
@@ -0,0 +1,26 @@
+using Xunit;
+
+namespace HaveFun
+{
+    public class CreateBoardSpec
+    {
+
+        [Fact]
+        public void CreatedBoardWillHavePlayersAtStart()
+        {
+            var board = new Board();
+            var player1Position = board.Player1Position;
+            Assert.Equal(0, player1Position);
+        }
+    }
+
+    public class Board
+    {
+        public Board()
+        {
+            
+        }
+
+        public int Player1Position { get; } = 0;
+    }
+}
