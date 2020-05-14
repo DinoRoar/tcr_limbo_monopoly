eeed8a8fb3929e0c6590db8aaa359b3d0478b60b
John Nicholas <john.nicholas@dinoroar.io>
2020-05-14 21:52:42 +0100

    automatically commit passing test

--- HaveFun/BankPropertySpec.cs
+++ HaveFun/BankPropertySpec.cs
@@ -5,16 +5,29 @@ namespace HaveFun
     public class BankPropertySpec
     {
         [Fact]
-        public void PLayerstartsWith500()
+        public void PLayerStartsWith500()
         {
-            var bank = new Bank();
+            var bank = new Bank(1);
             var playerMoney = bank.Player1Money;
             Assert.Equal(500, playerMoney);
+        }
+
+        [Fact]
+        public void Player2StartsWith500()
+        {
+            var bank = new Bank(2);
+            var money = bank.Player2Money;
+            Assert.Equal(500,money);
         }
     }
 
     public class Bank
     {
+        public Bank(int numberOfPlayers)
+        {
+        }
+
         public int Player1Money { get; } = 500;
+        public int Player2Money { get; } = 500;
     }
 }
